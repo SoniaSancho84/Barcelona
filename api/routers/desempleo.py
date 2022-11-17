15c112b8c5d29a24c9ec02002c5ee1ee1a9a3509
Sonia <soniasancho84@gmail.com>
2022-11-17 05:32:45 +0100

    creando routers

--- api/routers/desempleo.py
+++ api/routers/desempleo.py
@@ -1,14 +1,25 @@
-from fastapi import APIrouter
+from fastapi import APIRouter
 from database.mongo import db
 from bson import json_util
 from json import loads
 
-router=APIrouter()
+router=APIRouter()
 
 @router.get("/desempleo/{year}")
 def desempleo_by_year(year:int):
     filter = {"Year":year}
-    project = {}
-    res = db["desempleo"].find(filter, project)
-    return loads(json_util.dumpls(res))
+    project = {"_id":0}
+    res = db["Desempleo"].find(filter, project)
+    return loads(json_util.dumps(res))
 
+@router.get("/desempleo/year")
+def desempleo_by_year(year:int):
+    filter = {"Year":year}
+    project = {"_id":0}
+    res = db["Desempleo"].find(filter, project)
+    return loads(json_util.dumps(res))
+
+@router.get("/desempleo")
+def desempleo_by_all(dist:str,year:int, month:str):
+    res = db["Desempleo"].find({"District Name":dist, "Year":year, "Month":month}, {"_id":0})
+    return loads(json_util.dumps(res))
