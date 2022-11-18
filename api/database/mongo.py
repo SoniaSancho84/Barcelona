96246aa9cde94df0104aa09ec1cede67930b48d0
Sonia <soniasancho84@gmail.com>
2022-11-18 18:30:58 +0100

    Conexión api db

--- api/database/mongo.py
+++ api/database/mongo.py
@@ -6,4 +6,5 @@ load_dotenv()
 
 db = MongoClient(os.getenv("URL")).get_database("Barcelona")
 
-
+def desempleo_by_all(collection, filter = {}, project = {"_id":0}, limit = 0, skip = 0):
+    return db[collection].find(filter, project).limit(limit).skip(skip)
