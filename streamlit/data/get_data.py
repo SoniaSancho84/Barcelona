558dc9fc0195709e951da3ce540d975d49a1969d
Sonia <soniasancho84@gmail.com>
2022-11-21 06:10:42 +0100

    definiendo búsquedas

--- streamlit/data/get_data.py
+++ streamlit/data/get_data.py
@@ -2,8 +2,8 @@ import requests
 url = "http://127.0.0.1:8000"
 
 # buscar filtrando todos los parámetro
-def get_desempleo(year, month, district, barrio, gender):
-    return requests.get(url + f"/desempleo?year={year}&month={month}&district={district}&barrio={barrio}&gender={gender}").json()
+def get_desempleo(year, month, district, barrio, gender, status_desempleo):
+    return requests.get(url + f"/desempleo?year={year}&month={month}&district={district}&barrio={barrio}&gender={gender}&status={status_desempleo}").json()
 
 # buscar filtrando los años diferentes de la colección
 def get_years():
@@ -24,3 +24,20 @@ def get_barrio(district):
 # buscar filtrando por género
 def get_gender():
     return requests.get(url + "/gender/all").json()
+
+# buscar filtrando por estado
+def get_status():
+    return requests.get(url + "/status/all").json()
+
+# buscar total por año
+def desempleo_anual():
+    return requests.get(url + "/desempleo/total/year").json()
+
+
+# buscar total por género
+def desempleo_genero():
+    return requests.get(url + "/desempleo/total/genero").json()
+
+# buscar total por distrito
+def desempleo_por_distrito():
+    return requests.get(url + "/desempleo/total/distrito").json()
