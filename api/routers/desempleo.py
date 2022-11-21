630a0ffd75324f65708b715bb44f6619dc1738f2
Sonia <soniasancho84@gmail.com>
2022-11-21 06:08:30 +0100

    Añadiendo graficos

--- api/routers/desempleo.py
+++ api/routers/desempleo.py
@@ -17,8 +17,8 @@ def desempleo_by_year(year:int):
  
 # Filtrado por año, mes, distrito, barrio y género
 @router.get("/desempleo")
-def desempleo_by_all(year:int, month:str, district:str, barrio:str, gender:str):
-    filter = {"Year":year, "Month":month, "District Name": district, "Neighborhood Name":barrio, "Gender":gender}
+def desempleo_by_all(year:int, month:str, district:str, barrio:str, gender:str, status:str):
+    filter = {"Year":year, "Month":month, "District Name": district, "Neighborhood Name":barrio, "Gender":gender, "Demand_occupation":status}
     project = {"_id":0}
     res = db["Desempleo"].find(filter, project)
     return loads(json_util.dumps(res))
@@ -53,3 +53,27 @@ def barrios_disponibles(distrito):
 @router.get("/gender/all")
 def generos_disponibles():
     return ["Male", "Female"]
+
+# Retorna el estado de desempleo
+@router.get("/status/all")
+def estado_desempleo():
+    return ["Registered unemployed", "Unemployment demand"] 
+
+# Endpoint que devuelve el desempleo total de cada año
+@router.get("/desempleo/total/year")
+def desempleo_total_year():
+    res = db["Desempleo"].aggregate([{"$group": {"_id": "$Year", "desempleo_total":{"$sum":"$Number"}}}])
+    return loads(json_util.dumps(res))
+
+# Endpoint que devuelve el género
+@router.get("/desempleo/total/genero")
+def desempleo_total_genero():
+    res = db["Desempleo"].aggregate([{"$group": {"_id": "$Gender", "desempleo_total":{"$sum":"$Number"}}}])
+    return loads(json_util.dumps(res))
+
+# Endpoint que devuelve el desempleo por distrito
+@router.get("/desempleo/total/distrito")
+def desempleo_total_distrito():
+    res = db["Desempleo"].aggregate([{"$group": {"_id": "$District Name", "desempleo_total":{"$sum":"$Number"}}}])
+    return loads(json_util.dumps(res))
+
