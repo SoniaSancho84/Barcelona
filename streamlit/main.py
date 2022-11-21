d1d50a94c3be8a2252a1efd38485ca6a74e97be0
Sonia <soniasancho84@gmail.com>
2022-11-21 06:09:33 +0100

    definiendo opciones

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,17 +1,15 @@
 import streamlit as st
-from data.get_data import get_years, get_month, get_district, get_barrio, get_gender, get_desempleo 
+from page.busqueda import busqueda
+from page.grafica import graficas
 
 
-columna = st.sidebar.selectbox("Elegir página", ["Inicio", "Buscar Desempleo por filtrado"])
+columna = st.sidebar.selectbox("Elegir página", ["Inicio", "Buscar Desempleo por filtrado", "Graficas"])
 
 if columna == "Inicio":
-    st.title("Bienvenido a mi primer streamlit")
-elif columna == "Buscar Desempleo por filtrado":
-    st.title("Búsquedad de Desempleo")
-    op_year = st.selectbox("Elige el año en el que deseas buscar", get_years())
-    op_month = st.selectbox("Elige el mes en el que deseas buscar", get_month())
-    op_district = st.selectbox("Elige el distrito en el que deseas buscar", get_district())
-    op_barrio = st.selectbox("Elige el barrio en el que deseas buscar", get_barrio(op_district))
-    op_gender = st.selectbox("Elige el género en el que deseas buscar", get_gender())
-    respuesta = get_desempleo(op_year, op_month, op_district, op_barrio, op_gender)
-    st.write(respuesta)
+    st.title("Desempleo en Barcelona 2013-2017")
+    st.image("https://as01.epimg.net/diarioas/imagenes/2022/04/30/actualidad/1651323395_313362_1651323476_noticia_normal_recorte1.jpg")
+if columna == "Buscar Desempleo por filtrado":
+    busqueda()   
+
+if columna == "Graficas":
+    graficas()
